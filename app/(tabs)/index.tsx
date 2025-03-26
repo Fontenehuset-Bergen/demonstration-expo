846a87c42ffc01d105a8c0d26f4a423afec81fc8
AndyKodehode <andykodehode@gmail.com>
2025-03-26 13:09:16 +0100

    component and button

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,13 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Pressable, Image} from "react-native";
 
 export default function Index() {
   return (
     <View style={styles.container}>
       <Text>Her skal vi en vanlig start side</Text>
+      
+      <Pressable style={styles.pressableButton}>
+        <Text style={styles.buttonText}>Her er en tekst</Text>
+      </Pressable>
     </View>
   );
 }
@@ -14,4 +18,21 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
+
+  pressableButton: {
+    padding: 20,
+    width: 100,
+    height: 20,
+    borderStyle: 'solid',
+    borderWidth: 2,
+    borderRadius: 10,
+
+  },
+
+  buttonText:{
+    fontSize: 12,
+    fontStyle: 'normal',
+    color: 'black'
+  },
+
 });
---
+++ components/ui/card.tsx
@@ -0,0 +1,9 @@
+import { Text } from "react-native";
+
+
+export function Card(){
+    return(
+        <Text></Text>
+    )
+}
+
