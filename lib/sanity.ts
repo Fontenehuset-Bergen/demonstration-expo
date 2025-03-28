af7af0c708f5628c286b3368a43efa763c9c76e4
Kristoffer <kunkristoffer@gmail.com>
2025-03-28 12:12:08 +0100

    adding sanity fn

---
+++ lib/sanity.ts
@@ -0,0 +1,18 @@
+import { createClient } from "@sanity/client";
+
+export const client = createClient({
+  projectId: "xes78pqm",
+  dataset: "production",
+});
+
+export interface Drinks {
+    title: string
+    beskrivelse: string
+    allergens: string[]
+    image: string
+}
+
+export async function getPosts() {
+  const posts: Drinks[] = await client.fetch('*[_type == "kafe"] {title, beskrivelse, allergens, "image": image.asset->url}');
+  return posts;
+}
