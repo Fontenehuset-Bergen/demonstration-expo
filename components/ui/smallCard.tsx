33522ec1fc63a1680beb3c13ca5a21ad11b51e0e
Helga-Vi <katarina.jorgensen@gmail.com>
2025-04-04 13:11:43 +0200

    changing styling

--- components/ui/smallCard.tsx
+++ components/ui/smallCard.tsx
@@ -1,27 +1,65 @@
-import { Image, StyleSheet, Text, View } from "react-native";
+import React, { useState } from 'react';
+import { Image, StyleSheet, Text, View, ActivityIndicator } from "react-native";
 import { coffee } from "@/lib/sanity";
 
 export function SmallCard({ nameofcoffee, image, price }: coffee) {
+  const[isLoading, setIsLoading] = useState(true);
+
+  const handleImageLoad = () => {
+    setIsLoading(false);
+  }
+
   return (
-    <View style={style.container}>
-      <Image source={{ uri: image }} style={style.image} />
-      <Text>{nameofcoffee}</Text>
-      <Text>{price}</Text>
+    <View style={styles.container}>
+      {isLoading ? (
+        <ActivityIndicator size="large" color="#0000ff" />
+      ) : (
+      <Image 
+      source={{ uri: image }} 
+      style={styles.image} 
+      onLoad={handleImageLoad}
+      />
+    )}
+
+    <View style={styles.content}>
+      <Text style={styles.name}>{nameofcoffee}</Text>
+      <Text style={styles.price}>{price}</Text>
     </View>
+  </View>
   );
 }
 
-const style = StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
-    display: "flex",
-    flexDirection: "row",
-    width: "100%",
-    gap: 12,
-    alignItems: "center"
+    backgroundColor: '#f0f0f0',
+    borderRadius: 8,
+    overflow: 'hidden',
+    margin: 8,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
   },
   image: {
-    height: 100,
-    width: 100,
-    borderRadius: 8
+    height: 200,
+    width: '100%',
+    resizeMode: 'cover'
+  },
+  content: {
+    padding: 16,
+    flexGrow: 1,
+  },
+  name: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 8,
+  },
+  price: {
+    fontSize: 16,
+    color: '#666',
   }
 });
