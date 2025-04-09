d3d4b2370d35f65bc067080bceb3f4c52e8dd32c
Jan <jan@localhost.localdomain>
2025-04-09 14:53:29 +0000

    added new ui

--- app/(tabs)/placeholder.tsx
+++ app/(tabs)/placeholder.tsx
@@ -1,33 +1,40 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, ScrollView } from "react-native";
 import { Drinks, getPosts } from "@/lib/sanity";
 import { useEffect, useState } from "react";
 import { SmallCard } from "@/components/ui/smallCard";
 
 // Skift UniktNavn til noe du vill ha, BrukDenneNavneStilen
-export default function UniktNavn() {
+export default function Jan() {
   const [drikker, setDrikker] = useState<Drinks[]>([]);
 
   useEffect(() => {
     const results = async () => {
-      const result =  await getPost()
+	const result = await getPosts()
+	//code to return an array. Flatlist is best practice
+	if(result) {
+	    setDrikker(await getPosts());
+	} else {
+	    setDrikker([]);
+	}
     };
+
     results()
   }, []);
-
   return (
-    <View style={styles.container}>
+    <ScrollView>
       <Text>Dette er din personlige side</Text>
       <Text>her kan du gjøre mye gøy! 🥳</Text>
-      {drikker && drikker.map((drink) => <smallcard key={drink.title} title={drink.title} image={drink.image} />)}
-    </View>
+      {drikker && drikker.map((drink) => <SmallCard key={drink.title} title={drink.title} image={drink.image} />)}
+    </ScrollView>
   );
 }
 
 // Her kan du lese mer om styles: https://reactnative.dev/docs/style
 const styles = StyleSheet.create({
   container: {
-    flex: 2, // Flex 1 betyr at denne siden tar opp hele skjermen
+    flex: 1, // Flex 1 betyr at denne siden tar opp hele skjermen
     justifyContent: "center",
+    display: "flex",
     alignItems: "center",
   },
 });
---
+++ picture.tsx
@@ -0,0 +1,4 @@
+ 
+export function picture({image}: Drinks) {
+
+}
