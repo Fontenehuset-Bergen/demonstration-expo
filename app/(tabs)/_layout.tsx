79b35a3ad963cfbdf8d0759d74b0048b84d4a380
Stephanie <tephyreiso@proton.me>
2025-04-11 11:03:49 +0200

    Made design only affect personal page

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -4,7 +4,7 @@ import { Tabs } from "expo-router";
 
 export default function TabLayout() {
   return (
-    <Tabs screenOptions={{ tabBarActiveTintColor: "pink", tabBarStyle: { backgroundColor: "#1c1429" }, headerStyle: { backgroundColor: "#1c1429" }, headerTintColor: "pink", headerTitleStyle: { fontFamily: "monospace", fontWeight: 900 } }}>
+    <Tabs screenOptions={{ tabBarActiveTintColor: "blue" }}>
       <Tabs.Screen
         name="index"
         options={{
@@ -33,6 +33,11 @@ export default function TabLayout() {
         name="stephanie"
         options={{
           title: "Introduction",
+          tabBarStyle: { backgroundColor: "#1c1429" }, 
+          tabBarActiveTintColor: "pink",
+          headerStyle: { backgroundColor: "#1c1429" }, 
+          headerTintColor: "pink", 
+          headerTitleStyle: { fontFamily: "monospace", fontWeight: 900 },
           tabBarIcon: ({ color }) => (
             <FontAwesome size={28} name="child" color={color} />
           ),
